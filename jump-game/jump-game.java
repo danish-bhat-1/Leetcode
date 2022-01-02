92a87f73f0c09bbd262afa13a20a73ad84099800
Danish Nazir Bhatt <84076264+danishhhat960@users.noreply.github.com>
2022-01-02 11:07:56 +0530

    Time: 2 ms (75.65%), Space: 39.5 MB (85.63%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int N=nums.length;
-        int count;
-        count=0;
+        int count=0;
         for(int i =0;i<N;i++){
             if(i>count){
-                count=-1;
-               break;
+               //  count=-1;
+               // break;
+            return false;
             }
 			count=Math.max(count,i+nums[i]);
             
         }
-        return count==-1?false:true;
+        // return count==-1?false:true;
+        return true;
+        
     }
 }
